16301be695e4cd06df18874867a14c85486ceeb8
Daniel Brosseau <dab@catapulse.org>
2015-10-14 00:59:04 +0200

    move config from share/etc/ to .

--- lib/Catapulse/Web.pm
+++ lib/Catapulse/Web.pm
@@ -22,9 +22,9 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
-    'Plugin::ConfigLoader' => {
-        file => __PACKAGE__->path_to('share', 'etc'),
-    },
+   # 'Plugin::ConfigLoader' => {
+   #     file => __PACKAGE__->path_to('share', 'etc'),
+   # },
 );
 
 # Start the application
@@ -38,7 +38,28 @@ Catapulse::Web - Catalyst based CMS
 
 =head1 SYNOPSIS
 
+    # use autocreated database  share/catapulse-schema.db
     script/catapulse_server.pl
+
+    # or another DBIX dsn in share/etc/catapulse_web.yml
+    # and populate this database
+
+    # change Database Model in share/etc/catapulse_web.yml
+    # - Database Model -
+    Model::DBIC:
+     schema_class: Catapulse::Schema
+     connect_info:
+       dsn: dbi:mysql:database=catapulse;host=localhost
+       user: dbadmin
+       password: secret
+
+    # Populate database
+    ./script/populate_db.pl
+
+    # Start standalone server
+    ./script/catapulse_web.pl
+
+    # Go to http://localhost:3000
 
 =head1 DESCRIPTION
 
