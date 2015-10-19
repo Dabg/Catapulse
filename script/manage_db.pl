c4523dc66c5eeb405b292663be09f61f66b5a00e
Daniel Brosseau <dab@catapulse.org>
2015-10-19 19:14:29 +0200

    delete comments

--- script/manage_db.pl
+++ script/manage_db.pl
@@ -9,19 +9,3 @@ use DBIx::Class::Migration::Script;
 DBIx::Class::Migration::Script
     ->run_with_options(
         schema => Catapulse::Web->model('DBIC')->schema);
-
-
-# my $conffile = 'catapulse_web.yml';
-# -f $conffile or die "Error (conf: $conffile) : $!\n";
-
-# my $cfg = Config::Any->load_files({files => [ $conffile ], use_ext => 1 });
-# my ($filename, $conf) = %{$cfg->[0]};
-
-
-# die "'connect_info' was not found in $conffile"
-#     if ( ! defined $conf->{$model}->{connect_info});
-
-# my $connect_info = $conf->{$model}->{connect_info};
-# my $dsn = $connect_info->{dsn};
-
-# print "dsn=$dsn\n";
