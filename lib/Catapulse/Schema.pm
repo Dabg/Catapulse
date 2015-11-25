7a4027f44d8bcba738d8cec2a3e3bd62bb7c89fd
Daniel Brosseau <dab@catapulse.org>
2015-11-25 18:33:29 +0100

    typo

--- lib/Catapulse/Schema.pm
+++ lib/Catapulse/Schema.pm
@@ -1,9 +1,6 @@
 use utf8;
 package Catapulse::Schema;
 
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
-
 use strict;
 use warnings;
 
@@ -11,9 +8,6 @@ use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
-
-# Created by DBIx::Class::Schema::Loader v0.07043 @ 2015-10-19 18:59:01
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:1OEmPGpoyvMXMiEiLIF8cA
 
 BEGIN { $ENV{DBIC_OVERWRITE_HELPER_METHODS_OK} = 1 }
 
