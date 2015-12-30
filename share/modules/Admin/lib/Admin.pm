06175312d200081c268a26f88db658c92daf3be8
Daniel Brosseau <dab@catapulse.org>
2015-12-30 17:31:54 +0100

    replace users/roles fixtures by code

--- share/modules/Admin/lib/Admin.pm
+++ share/modules/Admin/lib/Admin.pm
@@ -1,13 +1,58 @@
 package Admin;
 
 use Moose;
-#with 'Catapulse::Module';
+use Authen::Passphrase::BlowfishCrypt;
+use DateTime;
 with 'CatalystX::InjectModule::Fixture';
+
+my $default_user = $ENV{USER} || 'unknown';
+
+my $admin = {
+           username => 'admin',
+           password => 'admin',
+           name     => $default_user,
+           created  => DateTime->now,
+           email     => "$default_user\@localhost",
+           tzone    => 'Europe/Paris',
+           website  => undef,
+           active   => 1,
+         };
+
+my $anonymous = {
+           username => 'anonymous',
+           password => 'bla',
+           name     => 'Anonymous',
+           created  => DateTime->now,
+           email    => 'anonymous@localhost',
+           tzone    => '',
+           website  => undef,
+           active   => 1,
+         };
 
 sub install {
     my ($self, $module, $mi) = @_;
 
-    $self->install_fixtures($module, $mi);
+    my $schema = $mi->ctx->model->schema;
+    # Add admin and anonymous users
+    my $u1 = $schema->resultset('User')->find_or_create($admin);
+    my $u2 = $schema->resultset('User')->find_or_create($anonymous);
+
+    $u1->add_to_roles( { name => 'admin'     } );
+    $u2->add_to_roles( { name => 'anonymous' } );
+}
+
+sub uninstall {
+    my ($self, $module, $mi) = @_;
+
+     my $schema = $mi->ctx->model->schema;
+
+    # Delete admin and anonymous users
+    $schema->resultset('User')->search( { username => 'admin'     })->delete_all;
+    $schema->resultset('User')->search( { username => 'anonymous' })->delete_all;
+
+    # Delete admin and anonymous roles
+    $schema->resultset('Role')->search( { name => 'admin'     })->delete_all;
+    $schema->resultset('Role')->search( { name => 'anonymous' })->delete_all;
 }
 
 1;
