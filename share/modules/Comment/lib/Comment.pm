e62866f7e7bfbbf080911f1022d64a15a06832e1
Daniel Brosseau <dab@catapulse.org>
2016-01-10 12:50:20 +0100

    move Comment operation fixture to install method

--- share/modules/Comment/lib/Comment.pm
+++ share/modules/Comment/lib/Comment.pm
@@ -12,6 +12,21 @@ my $block = {
             => undef
         };
 
+my $operations = [
+    {
+        active => 1,
+        name   => 'view_Comment'
+    },
+    {
+        active => 1,
+        name   => 'add_Comment'
+    },
+    {
+        active => 1,
+        name   => 'delete_Comment'
+    },
+];
+
 sub install {
     my ($self, $module, $mi) = @_;
 
@@ -22,6 +37,12 @@ sub install {
     my $main_template = $schema->resultset('Template')->search( { name => 'Main' } )->first;
     $main_template->add_to_blocks( $block )
         or die "Cannot add Comment block to Main Template";
+
+
+    foreach my $op ( @$operations ) {
+        $schema->resultset('Operation')->find_or_create($op);
+    }
+
 }
 
 sub uninstall {
