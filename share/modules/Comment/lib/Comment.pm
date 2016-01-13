b9ac915e12bdf89255368adcd06c7d62f5c705c4
Daniel Brosseau <dab@catapulse.org>
2016-01-13 19:01:41 +0100

    uninstall comment

--- share/modules/Comment/lib/Comment.pm
+++ share/modules/Comment/lib/Comment.pm
@@ -44,8 +44,7 @@ sub install {
     # Add Comment block to template Main
     $mi->log("  - add Comment block to Main template");
     my $main_template = $schema->resultset('Template')->search( { name => 'Main' } )->first;
-    $main_template->add_to_blocks( $block )
-        or die "Cannot add Comment block to Main Template";
+    $main_template->add_to_blocks( $block );
 
     # Add Operation ( add_Comment, view_Comment, delete_Comment)
     foreach my $op ( @$operations ) {
@@ -59,11 +58,9 @@ sub uninstall {
 
     my $schema = $mi->ctx->model->schema;
     $mi->log("  - delete Comment block");
-    $schema->resultset('Typeobj')->search({ name => $typeobj->{name} })->delete_all
-        or die "Cannot delete Comment Typeobj";
+    $schema->resultset('Typeobj')->search({ name => $typeobj->{name} })->delete_all;
 
-    $schema->resultset('Block')->search({ name => $block->{name} })->delete_all
-        or die "Cannot delete Comment block";
+    $schema->resultset('Block')->search({ name => $block->{name} })->delete_all;
 }
 
 1;
