c428b1d86695d70f03e6b0b8a4ff35344b75ab15
Daniel Brosseau <dab@catapulse.org>
2016-01-14 19:19:56 +0100

    move wikipage fixture to wiki installer

--- share/modules/Wiki/lib/Wiki.pm
+++ share/modules/Wiki/lib/Wiki.pm
@@ -26,6 +26,53 @@ my $new_operations = [
     },
 ];
 
+my $wiki_pages = [
+    {
+        active     => 1,
+        created    => '',
+        id         => 1,
+        name       => '/',
+        parent_id  => 0,
+        template   => 1,
+           title   => 'index',
+           type    => 2,
+           version => 1
+         },
+    {
+        active     => 1,
+        created    => '',
+        id         => 2,
+        name       => 'default',
+        parent_id  => 1,
+        template   => 1,
+        title      => 'default',
+        type       => 2,
+        version    => 1
+    },
+    {
+        active     => 1,
+        created    => '',
+        id         => 3,
+        name       => 'admin',
+        parent_id  => 1,
+        template   => 1,
+        title      => 'Administration',
+        type       => 2,
+        version    => 1
+    },
+    {
+        active     => 1,
+        created    => '',
+        id         => 4,
+        name       => 'Help',
+        parent_id  => 1,
+        template   => 1,
+        title      => 'Help',
+        type       => 2,
+        version    => 1
+    },
+];
+
 sub install {
     my ($self, $module, $mi) = @_;
 
@@ -40,6 +87,12 @@ sub install {
     foreach my $op ( @$new_operations ) {
         $mi->log("    - add $op->{name} Operation");
         $schema->resultset('Operation')->find_or_create($op);
+    }
+
+    # Add Wiki Page ( add_Comment, view_Comment, delete_Comment)
+    foreach my $p ( @$wiki_pages ) {
+        $mi->log("    - add $p->{title} Page");
+        $schema->resultset('Page')->find_or_create($p);
     }
 }
 
