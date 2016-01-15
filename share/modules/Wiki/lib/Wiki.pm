f955a0218cc4dce9e1d596de290114d15cb7be98
Daniel Brosseau <dab@catapulse.org>
2016-01-15 18:34:00 +0100

    move permission fixture to Wiki installer

--- share/modules/Wiki/lib/Wiki.pm
+++ share/modules/Wiki/lib/Wiki.pm
@@ -34,9 +34,9 @@ my $wiki_pages = [
         name       => '/',
         parent_id  => 0,
         template   => 1,
-           title   => 'index',
-           type    => 2,
-           version => 1
+        title      => 'index',
+        type       => 2,
+        version    => 1
          },
     {
         active     => 1,
@@ -73,6 +73,18 @@ my $wiki_pages = [
     },
 ];
 
+my $ops_to_access_pages = [
+    {
+        obj_id       => 1,
+        operation_id => 6,
+        typeobj_id   => 1
+    },
+    {
+        obj_id       => 3,
+        operation_id => 6,
+        typeobj_id   => 1
+    },
+];
 sub install {
     my ($self, $module, $mi) = @_;
 
@@ -83,16 +95,66 @@ sub install {
     $schema->resultset('Pagetype')->find_or_create($pagetype);
 
 
-    # Add Operation ( add_Comment, view_Comment, delete_Comment)
+    # Add Operation (  edit_Page )
     foreach my $op ( @$new_operations ) {
         $mi->log("    - add $op->{name} Operation");
         $schema->resultset('Operation')->find_or_create($op);
     }
 
-    # Add Wiki Page ( add_Comment, view_Comment, delete_Comment)
+    # Add Wiki Page
+    my $pages = {};
     foreach my $p ( @$wiki_pages ) {
         $mi->log("    - add $p->{title} Page");
-        $schema->resultset('Page')->find_or_create($p);
+        $p->{created} = DateTime->now;
+        my $rs = $schema->resultset('Page')->find_or_create($p);
+        $pages->{$rs->title} = $rs;
+    }
+
+
+    # Protect some Wiki Page
+    foreach my $p ( @$ops_to_access_pages ) {
+        $mi->log("    - protect Page $p->{obj_id}");
+        $schema->resultset('ObjOperation')->find_or_create($p);
+    }
+
+    # Add permissions
+    my $typepage = $schema->resultset('Typeobj')->search({ name => 'Page'})->first;
+    my $op_view_page = $schema->resultset('Operation')->search({ name => 'view_Page'})->first;
+    my $op_perm_page = $schema->resultset('Operation')->search({ name => 'permission_Page'})->first;
+
+    my $role_admin     = $schema->resultset('Role')->search({ name => 'admin'})->first;
+    my $role_anonymous = $schema->resultset('Role')->search({ name => 'anonymous'})->first;
+
+    # Admin and anonymous can view page / (index)
+    my $perms =[
+        # Admin can 'view' on page / (recursively)
+        { role_id => $role_admin->id,
+          typeobj_id => $typepage->id,
+          obj_id => $pages->{index}->id,
+          operation_id => $op_view_page->id,
+          value => 1,
+          inheritable => 1
+      },
+        # Admin can permission on page / (recursively)
+        { role_id => $role_admin->id,
+          typeobj_id => $typepage->id,
+          obj_id => $pages->{index}->id,
+          operation_id => $op_perm_page->id,
+          value => 1,
+          inheritable => 1
+      },
+        # Anonymous can 'view' on page / (recursively)
+        { role_id => $role_anonymous->id,
+          typeobj_id => $typepage->id,
+          obj_id => $pages->{index}->id,
+          operation_id => $op_view_page->id,
+          value => 1,
+          inheritable => 1
+      },
+    ];
+    foreach my $p ( @$perms ) {
+        $mi->log("    - add permission $p->id");
+        $schema->resultset('Permission')->find_or_create($p);
     }
 }
 
