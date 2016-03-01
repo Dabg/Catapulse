98bbbb748a49898df9eee3803c02407b57898787
Daniel Brosseau <dab@catapulse.org>
2016-03-01 19:19:43 +0100

    delete unnecessaries block keys id, parent_id, position

--- share/modules/Block/lib/Block.pm
+++ share/modules/Block/lib/Block.pm
@@ -7,72 +7,37 @@ my $blocks = [
     {
         active => 1,
         file   => 'blocks/querylog.tt',
-        id     => 10,
         name   => 'querylog',
-        parent_id
-            => 0,
-        position
-            => undef
         },
     {
         active => 1,
         file   => 'blocks/navbar.tt',
-        id     => 1,
         name   => 'navbar',
-        parent_id
-            => 0,
-        position
-            => undef
         },
     {
         active => 1,
         file   => 'blocks/breadcrumbs.tt',
-        id     => 2,
         name   => 'breadcrumbs',
-        parent_id
-            => 0,
-        position
-            => undef
         },
     {
         active => 1,
         file   => 'blocks/messages.tt',
-        id     => 3,
         name   => 'message',
-        parent_id
-            => 0,
-        position
-            => undef
         },
     {
         active => 1,
         file   => 'blocks/sidebar.tt',
-        id     => 6,
         name   => 'sidebar',
-        parent_id
-            => 0,
-        position
-            => undef
         },
     {
         active => 1,
         file   => 'blocks/footer.tt',
-        id     => 7,
         name   => 'footer',
-        parent_id
-            => 0,
-        position
-            => undef
         },
     {
         active => 1,
         file   => 'blocks/links.tt',
-        id     => 8,
         name   => 'links',
-        parent_id
-            => 0,
-        position
-            => undef
         },
 ];
 
